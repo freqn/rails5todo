773bda3381bf0296b6fb39e955b9c6649c01a6ab
freqn <ceyler@gmail.com>
2018-12-17 20:55:21 -0500

    min input value

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,23 @@ class GenData
 
   def run 
     Todo.create([
-      {title: "status handling", description: "to mark as 'done' instead of destroying", priority_level: randomize },
-      {title: "More styles", description: "Bootstrap components", priority_level: randomize },
-      {title: "user registration", description: "Compare Devise vs alternatives", priority_level: randomize },
-      {title: "user settings/profiles", description: "Users should be able to edit their own profiles", priority_level: randomize },
-      {title: "user level privileges", description: "Admins should be able to manage users and their todos", priority_level: randomize },
-      {title: "metrics", description: "Users should have metrics", priority_level: randomize },
-      {title: "admin/super admin", description: "task and user mgt", priority_level: randomize },
-      {title: "alerts", description: "action cable?", priority_level: randomize },
-      {title: "slack integration", description: "API feed", priority_level: randomize },
-      {title: "ajax", description: "more realtime goodness", priority_level: randomize },
+      { title: "status handling", description: "to mark as 'done' instead of destroying", priority_level: randomize },
+      { title: "more styles", description: "Bootstrap components", priority_level: randomize },
+      { title: "user registration", description: "Compare Devise vs alternatives", priority_level: randomize },
+      { title: "user settings/profiles", description: "Users should be able to edit their own profiles", priority_level: randomize },
+      { title: "user level privileges", description: "Admins should be able to manage users and their todos", priority_level: randomize },
+      { title: "metrics", description: "Users should have metrics", priority_level: randomize },
+      { title: "admin/super admin", description: "task and user mgt", priority_level: randomize },
+      { title: "alerts", description: "action cable?", priority_level: randomize },
+      { title: "slack integration", description: "API feed", priority_level: randomize },
+      { title: "ajax", description: "more realtime goodness", priority_level: randomize },
     ])
   end
 
   private
 
   def randomize 
-    rand(1..10)
+    rand(1..5)
   end
 end
 
