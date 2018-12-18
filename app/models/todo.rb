6e718e92ea6167869b3e718104a56defed01f65c
freqn <ceyler@gmail.com>
2018-12-17 20:25:24 -0500

    refactor priority algorithm

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,10 +2,8 @@ class Todo < ApplicationRecord
   validates :title, :description, :priority_level, presence: true
 
   def self.priority_counts
-    list = self.pluck(:priority_level)
-    result = list.each_with_object(Hash.new(0)) do |level,count|
-              count[level] += 1
-            end.select { |k,v| v > 1 }
-    result.sort_by { |k, v| k }.reverse
+    levels, result = self.pluck(:priority_level), Hash.new(0)
+    levels.each_with_object(result) { |level,count| count[level] += 1 }
+    result.select { |k,v| v > 1 }.sort_by { |k, v| k }.reverse
   end
 end
